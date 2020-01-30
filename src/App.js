839235feb316ad58b2b69e895615d8216329f2fe
Aman <aman_A00@mail.ru>
2020-01-30 14:53:46 +0600

    sdvds

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ClustererCreate from "./map";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+        <ClustererCreate />
     </div>
   );
 }
