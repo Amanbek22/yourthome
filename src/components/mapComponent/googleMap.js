ce4d995aada519a6c6a2d0aaa93b718f465adf7b
Aman <aman_A00@mail.ru>
2020-02-19 00:40:51 +0600

    post

--- src/components/mapComponent/googleMap.js
+++ src/components/mapComponent/googleMap.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import {GoogleMap, InfoWindow, Marker, withGoogleMap, withScriptjs} from "react-google-maps";
-import someData from '../../point.json'
 import css from './map.module.css'
 import appartment from '../../img/room.png'
 import axios from "axios";
@@ -35,7 +34,7 @@ const MyMapComponent = withScriptjs(withGoogleMap((props) => {
                                 newarr.push(item.props.id)
                                 // console.log(newarr)
                             } else {
-
+                                console.log("failed")
                             }
                         }))
                         props.setVisibleMarkers(newarr)
@@ -50,11 +49,11 @@ const MyMapComponent = withScriptjs(withGoogleMap((props) => {
                                 ref={onMarkerMounted}
                                 onClick={() => setSelectedPark(item)}
                                 position={{
-                                    lat: item.geometry.coordinates[0],
-                                    lng: item.geometry.coordinates[1]
+                                    lat: item.latitude,
+                                    lng: item.longitude
                                 }}
 
-                                title={item.name}
+                                title={item.description}
                                 text={item.price}
                                 key={item.id}
                                 id={item.id}
@@ -65,8 +64,8 @@ const MyMapComponent = withScriptjs(withGoogleMap((props) => {
                     </MarkerClusterer>
                     {selectedPark && (
                         <InfoWindow position={{
-                            lat: selectedPark.geometry.coordinates[0],
-                            lng: selectedPark.geometry.coordinates[1]
+                            lat: selectedPark.latitude,
+                            lng: selectedPark.longitude
                         }}
                                     onCloseClick={() => setSelectedPark(null)}
                         >
@@ -88,35 +87,82 @@ const MyMapComponent = withScriptjs(withGoogleMap((props) => {
 ))
 
 const WrapperMap = props => {
+    console.log(props.points.points)
     useEffect(() => {
-        props.setPoint(someData.features)
+        // props.setPoint(someData.features)
         axios.get("https://yourthomeneobis2.herokuapp.com/announcements/")
             .then(res=>{
-                console.log(res)
+                console.log(res.data)
+                props.setPoint(res.data)
             })
     }, []);
     const [selected, setSelected] = useState([])
+    const [address,setAddress] = useState()
     const selectedPark = item => {
         setSelected(item)
     }
     const pushLocation = e => {
-        let add = prompt("input your addres?", "");
         let latlng = [e.latLng.lat(), e.latLng.lng()];
-        console.log("" + latlng[0],+ "\n"+latlng[1]);
-        //let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latlng[0]},${latlng[1]}&key=AIzaSyDMDrqHrfbKWIzQDmnxHl2WcJGAnAgUX0A`
         let newurl = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latlng[0]}&lon=${latlng[1]}&accept-language=ru`
         axios.get(newurl)
-            .then(res => alert(res.data.display_name));
+            .then(res =>{
+                // alert(res.data.display_name)
+                let addresss = res.data.address;
+                setAddress(addresss)
+            })
+        let add = prompt("input your description?", "");
+        //let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latlng[0]},${latlng[1]}&key=AIzaSyDMDrqHrfbKWIzQDmnxHl2WcJGAnAgUX0A`
+
         let items = {
-            name: add,
-            type: "Feature",
-            geometry: {
-                type: "Point",
-                coordinates: latlng
-            },
-            id: Math.random()
-        }
-        props.addPoints([items])
+                id: props.points.points.length + 1,
+                type: "квартира",
+                room: "3",
+                address: {
+                    id: 1,
+                    house_number: "1",
+                    street: "1",
+                    city: "1",
+                    postcode: "1",
+                    country: "1",
+                    country_code: "1"
+                },
+                square: 45.0,
+                date_of_arrival: "2020-02-18",
+                date_of_departure: "2020-02-18",
+                price: 1500,
+                description: add,
+                status: false,
+                latitude: latlng[0],
+                longitude: latlng[1],
+                pub_date: "2020-02-18T20:15:56.439719+06:00",
+                images: [],
+                owner: "na za\b\b, "
+            }
+        // props.addPoints([items])
+        axios.post("https://yourthomeneobis2.herokuapp.com/apartment",{
+            "type": 4,
+            "room": "3",
+            "square": 55,
+            "date_of_arrival": "2020-02-20",
+            "date_of_departure": "2020-02-22",
+            "price": 30000,
+            "description": "это квартира.",
+            "status": true,
+            "images": [],
+            "owner": 1,
+            "latitude": 34.3456789,
+            "longitude": 33.3456789,
+            "address": {
+                "house_number": 6,
+                "street": "Абая",
+                "city": "Бишкек",
+                "postcode": "722011",
+                "country": "Кыргызстан",
+                "country_code": "kg"
+            }
+        }).then(res=>{
+            console.log(res)
+        })
     }
     let arr = [];
     selected.map(id => arr.push(...props.points.points.filter(item => item.id === id)));
@@ -126,13 +172,12 @@ const WrapperMap = props => {
             <div>
                 <Element
                     img={roomsImg}
-                    forSale={item.name}
-                    address={"г. Ташкент, Алмазарский район, Чиланзар-1/4"}
+                    forSale={item.description}
+                    address={item.address.house_number}
                     area={75}
                     room={3}
                     floor={"5/9"}
                     addetDate={"Вчера"}
-                    saved={item.saved}
                     url={""}
                 />
             </div>
